0ea7b395f41167a0f84d86d9a95ff5273e978b3c
Anuj Verma <anujvermagkp@gmail.com>
2024-11-22 15:30:22 +0530

    deleted:    DATABASE FILE/01 LOGIN DETAILS & PROJECT INFO.txt 	deleted:    DATABASE FILE/ecommerceweb.sql 	deleted:    admin/inc/01 LOGIN DETAILS & PROJECT INFO.txt 	deleted:    assets/01 LOGIN DETAILS & PROJECT INFO.txt 	deleted:    assets/img/01 LOGIN DETAILS & PROJECT INFO.txt 	deleted:    assets/uploads/01 LOGIN DETAILS & PROJECT INFO.txt 	modified:   cart.php 	modified:   checkout.php 	new file:   checkout1.php 	new file:   customer-billing-shipping-update.php 	new file:   customer-order.php 	new file:   customer-password-update.php 	new file:   customer-profile-update.php 	new file:   customer-sidebar.php 	new file:   dashboard.php 	modified:   header.php 	modified:   login.php 	new file:   logout.php 	modified:   product-category.php 	modified:   product.php 	modified:   registration.php

--- login.php
+++ login.php
@@ -1,50 +1,91 @@
 <?php require_once('header.php'); ?>
+<!-- fetching row banner login -->
+<?php
+$statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+foreach ($result as $row) {
+    $banner_login = $row['banner_login'];
+}
+?>
+<!-- login form -->
+<?php
+if(isset($_POST['form1'])) {
+        
+    if(empty($_POST['cust_email']) || empty($_POST['cust_password'])) {
+        $error_message = LANG_VALUE_132.'<br>';
+    } else {
+        
+        $cust_email = strip_tags($_POST['cust_email']);
+        $cust_password = strip_tags($_POST['cust_password']);
 
-<!-- Banner Section -->
-<div class="page-banner" style="background-color:#444;background-image: url(assets/uploads/banner-login.jpg);">
+        $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_email=?");
+        $statement->execute(array($cust_email));
+        $total = $statement->rowCount();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        foreach($result as $row) {
+            $cust_status = $row['cust_status'];
+            $row_password = $row['cust_password'];
+        }
+
+        if($total==0) {
+            $error_message .= LANG_VALUE_133.'<br>';
+        } else {
+            //using MD5 form
+            if( $row_password != md5($cust_password) ) {
+                $error_message .= LANG_VALUE_139.'<br>';
+            } else {
+                if($cust_status == 0) {
+                    $error_message .= LANG_VALUE_148.'<br>';
+                } else {
+                    $_SESSION['customer'] = $row;
+                    header("location: ".BASE_URL."dashboard.php");
+                }
+            }
+            
+        }
+    }
+}
+?>
+
+<div class="page-banner" style="background-color:#444;background-image: url(assets/uploads/<?php echo $banner_login; ?>);">
     <div class="inner">
         <h1><?php echo LANG_VALUE_10; ?></h1>
     </div>
 </div>
 
-<!-- Login Form Section -->
 <div class="page">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="user-content">
-                    <form action="" method="post">                  
 
+                    
+                    <form action="" method="post">
+                                         
                         <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-4">
-                                <!-- Error and Success Messages (displayed if set) -->
-                                <div id="error-message" style="padding: 10px;background:#f1f1f1;margin-bottom:20px;display:none;">
-                                    <!-- Error message will be inserted here -->
-                                </div>
-                                <div id="success-message" style="padding: 10px;background:#f1f1f1;margin-bottom:20px;display:none;">
-                                    <!-- Success message will be inserted here -->
-                                </div>
-                                
-                                <!-- Email Input -->
+                                <?php
+                                if($error_message != '') {
+                                    echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
+                                }
+                                if($success_message != '') {
+                                    echo "<div class='success' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$success_message."</div>";
+                                }
+                                ?>
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_94; ?> *</label>
                                     <input type="email" class="form-control" name="cust_email">
                                 </div>
-
-                                <!-- Password Input -->
                                 <div class="form-group">
                                     <label for=""><?php echo LANG_VALUE_96; ?> *</label>
                                     <input type="password" class="form-control" name="cust_password">
                                 </div>
-
-                                <!-- Submit Button -->
                                 <div class="form-group">
                                     <label for=""></label>
                                     <input type="submit" class="btn btn-success" value="<?php echo LANG_VALUE_4; ?>" name="form1">
                                 </div>
-
-                                <!-- Forgot Password Link -->
                                 <a href="forget-password.php" style="color:#e4144d;"><?php echo LANG_VALUE_97; ?>?</a>
                             </div>
                         </div>                        
